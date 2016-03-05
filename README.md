8fd18c145b8fa4e25e9ca62df83b8202cbc9e2a7
Colin Fausnaught <cfausn@gmail.com>
2016-03-05 17:46:35 -0500

    testing

---
+++ README.md
@@ -0,0 +1,7 @@
+# brickhack2
+Brickhack repo
+
+
+BrickHack2 (2016): TamagotchSSE
+
+TamagotchSSE is a fun, interactive game where you raise and evolve an SSE Sprite.
---
+++ mobile/src/main/java/edu/rit/sse/brickhack/MainActivity.java
@@ -0,0 +1,14 @@
+// List Packages here:
+package edu.rit.sse.brickhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
