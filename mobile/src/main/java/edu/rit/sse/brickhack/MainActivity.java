09a5cd35707afaf2cfa753fb24d140b1b2eedcdf
Colin Fausnaught <cfausn@gmail.com>
2016-03-05 19:52:11 -0500

    test

--- mobile/src/main/java/edu/rit/sse/brickhack/MainActivity.java
+++ mobile/src/main/java/edu/rit/sse/brickhack/MainActivity.java
@@ -5,17 +5,21 @@ package edu.rit.sse.brickhack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static ImageView sprite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        sprite = (ImageView)findViewById(R.id.sprite);
+
+
     }
-
-
 
 
 }
