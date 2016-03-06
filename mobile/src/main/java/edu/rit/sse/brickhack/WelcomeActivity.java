043a10a3a99920a380294615a0bc33409f689bd3
Kayla Nussbaum <kayla.nussbaum@live.com>
2016-03-05 22:23:58 -0500

    progress on welcome activity

--- mobile/src/main/java/edu/rit/sse/brickhack/WelcomeActivity.java
+++ mobile/src/main/java/edu/rit/sse/brickhack/WelcomeActivity.java
@@ -3,21 +3,25 @@ package edu.rit.sse.brickhack;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by Kayla on 3/5/2016.
  */
 public class WelcomeActivity extends AppCompatActivity {
 
-    private static ImageView sprite;
+    private static LinearLayout wallpaper_main;
+    private TextView TamagotchSSE;
+    private int time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        sprite = (ImageView)findViewById(R.id.sprite);
-
+        wallpaper_main = (LinearLayout)findViewById(R.id.wallpaper_main);
+        TamagotchSSE = (TextView)findViewById(R.id.TamagotchSSE);
 
     }
 
